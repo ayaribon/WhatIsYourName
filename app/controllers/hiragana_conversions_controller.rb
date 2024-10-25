2726e405ad01b43832c2de2f05a2d89fed60ade9
ayaribon <kubotaren5213@docomo.ne.jp>
2024-10-25 21:56:56 +0900

    add:ログイン、ログアウト機能

--- app/controllers/hiragana_conversions_controller.rb
+++ app/controllers/hiragana_conversions_controller.rb
@@ -1,2 +1,4 @@
 class HiraganaConversionsController < ApplicationController
+
+  def new; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,13 +15,13 @@ Rails.application.routes.draw do
   root "static_pages#top"
   
   # ログイン関連のルート
-  get 'login', to: 'user_sessions#new', as: 'login'
+  get 'login', to: 'user_sessions#new', as: :login
   post 'login', to: 'user_sessions#create' 
-  delete 'logout', to: 'user_sessions#destroy', as: 'logout'
+  delete 'logout', to: 'user_sessions#destroy', as: :logout
 
   # 新規登録関連のルート
   resources :users, only: [:new, :create]
 
   # ひらがな変換関連のルート
-  resources :hiragana_conversions, only: [:new, :create, :show]
+  resources :hiragana_conversions, only: [:hiragana_input, :new, :create, :show]
 end
